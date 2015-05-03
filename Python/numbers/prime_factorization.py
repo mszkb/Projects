403c748b9ba82508f72b483305ec52064c6563fb
msz <martin.szalay.kb@gmail.com>
2015-05-03 18:54:17 +0200

    [SOLVED] Prime Factorization

---
+++ Python/numbers/prime_factorization.py
@@ -0,0 +1,20 @@
+__author__ = 'Martin'
+
+
+# Source: http://stackoverflow.com/a/16996439
+
+def primes(n):
+    primfac = []
+    d = 2
+    while d*d <= n:
+        while (n % d) == 0:
+            primfac.append(d)  # supposing you want multiple factors repeated
+            n //= d
+        d += 1
+    if n > 1:
+       primfac.append(n)
+    return primfac
+
+
+if __name__ == '__main__':
+    print(primes(80))
